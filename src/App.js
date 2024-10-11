c54b496fddd70c8ae437ef7bf42c2959aa31c1b0
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-10-12 11:30:09 +1300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo img-fluid" alt="Dictionary Logo" />
       </header>
+      <h1>Dictionary</h1>
+      <Dictionary />
     </div>
   );
 }
