0670333e90303685945e3fce575a4d17bbf8f581
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-10-13 10:12:30 +1300

    Made component for the meaning, looped through object and rendered

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
-  let [word, setWord] = useState(null);
+  let [word, setWord] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function searchWord(event) {
     event.preventDefault();
-    alert(`Searching for ${word} definiton`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -26,6 +27,7 @@ export default function Dictionary() {
         <input type="search" placeholder="Enter a Word..." onChange={getWord} />
         <input type="submit" value="Search" className="btn btn-dark" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
