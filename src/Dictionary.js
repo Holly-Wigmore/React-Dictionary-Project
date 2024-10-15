405cfbf630e63d336ad134feb2656fdb0b4489e4
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-10-15 19:07:13 +1300

    Added an image api and styled it to be a grid

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [word, setWord] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imageApiKey = "754b84e4ec96a5481tfao024db3b3aff";
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${imageApiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
   }
 
   function searchWord(event) {
@@ -47,6 +56,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div key={index} className="col-4">
+                <a href={photo.url} target="_blank" rel="noreferrer">
+                  <img
+                    className="img-fluid"
+                    src={photo.src.landscape}
+                    alt={photo.alt}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
